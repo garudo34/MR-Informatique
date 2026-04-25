17b47f6421dd5812136ea4b55f16a4e193c6b4d7
Fabien <fabien.peyres@free.fr>
2026-04-25 22:46:54 +0200

    optimisation cache appel api google

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from 'next/server'
 
+export const revalidate = 604800
+
 const PLACE_ID = process.env.GOOGLE_PLACE_ID!
 const API_KEY = process.env.GOOGLE_API_KEY!
 
 export async function GET() {
   try {
-    const url = `https://places.googleapis.com/v1/places/${PLACE_ID}?fields=displayName,rating,reviews,userRatingCount&key=${API_KEY}`
+    const url = `https://places.googleapis.com/v1/places/${PLACE_ID}?fields=displayName,rating,reviews,userRatingCount`
 
     const res = await fetch(url, {
       headers: {
         'Content-Type': 'application/json',
+        'X-Goog-Api-Key': API_KEY,
       },
-      cache: 'force-cache',
       next: { revalidate: 604800 }, // 1week in seconds
     })
 
@@ -27,7 +29,7 @@ export async function GET() {
       userRatingCount: data.userRatingCount,
       reviews: data.reviews || [],
     })
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { error: 'Impossible de récupérer les avis' },
       { status: 500 }
